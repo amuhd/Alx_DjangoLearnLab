ad2c9919a2d417dd489e42176797d5c9f6e66485
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-08-17 14:32:34 +0300

    tmp

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 from .models import Book 
 
+def list_books(request):
+  template = loader.get_template('list_books.html')
+  return HttpResponse(template.render())
+
+def library_detail(request):
+  template = loader.get_template('library_detail.html')
+  return HttpResponse(template.render())
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-def library_details(request, library_id):
+def library_detail(request, library_id):
     library = Library.objects.get(id=library_id)
     return render(request, 'relationship_app/library_detail.html', {'library': library})
 
