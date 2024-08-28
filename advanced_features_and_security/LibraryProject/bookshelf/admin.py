a745ce2be613643f349cfba67ee8434a4a77de15
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-08-28 22:31:25 +0300

    AFS

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomUser, CustomUserAdmin
 
 # Customizing admin.
 class BookAdmin(admin.ModelAdmin):
@@ -9,3 +10,4 @@ class BookAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Book)
+admin.site.register(CustomUser, CustomUserAdmin)
