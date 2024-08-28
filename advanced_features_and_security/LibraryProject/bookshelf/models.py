924a12b15944bf85109fda02a5afb7e924504af0
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-08-28 23:49:40 +0300

    t2

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -54,3 +54,18 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+# bookshelf/models.py
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can create model instances"),
+            ("can_delete", "Can delete model instances"),
+        ]
