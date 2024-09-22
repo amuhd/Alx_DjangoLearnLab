21bf7b2820c9fed876746fef3feafe16321bc0bd
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-09-22 14:53:22 +0300

    T0

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,17 +1,24 @@
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import CustomUser
+from .models import User
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'password', 'bio', 'profile_picture', 'followers']
 
     def create(self, validated_data):
         # Hash the password before saving
         validated_data['password'] = make_password(validated_data['password'])
-        user = CustomUser(**validated_data)
+        user = User(**validated_data)
         user.save()
+        
+        Token.objects.create(user=user)
+        
         return user
