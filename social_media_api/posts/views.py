fd52c24c49a75d3b122c0582c01af051d4ac8811
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-09-22 17:17:07 +0300

    T3

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -50,32 +50,40 @@ class UserFeed(generics.ListAPIView):
 
 from rest_framework import generics, permissions
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import Post, Like
 from notifications.models import Notification
-from notifications.serializers import NotificationSerializer  # Create this serializer
 from accounts.models import CustomUser
-from django.contrib.contenttypes.models import ContentType 
+from django.contrib.contenttypes.models import ContentType
 
 class LikePost(generics.CreateAPIView):
-    queryset = Like.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        post = Post.objects.get(pk=self.kwargs['pk'])  # Get the post
-        serializer.save(user=self.request.user, post=post)  # Save the like
+    def post(self, request, pk, *args, **kwargs):
+        post = get_object_or_404(Post, pk=pk)  # Retrieve the post
+        like, created = Like.objects.get_or_create(user=request.user, post=post)  # Like or get existing
 
-        # Create a notification
-        Notification.objects.create(
-            recipient=post.author,  # Notify the post author
-            actor=self.request.user,
-            verb='liked your post',
-            target_content_type=ContentType.objects.get_for_model(Post),
-            target_object_id=post.id
-        )
+        if created:
+            # Create a notification if a new like is created
+            Notification.objects.create(
+                recipient=post.author,
+                actor=request.user,
+                verb='liked your post',
+                target_content_type=ContentType.objects.get_for_model(Post),
+                target_object_id=post.id
+            )
+            return Response({"message": "Post liked."}, status=201)
+        else:
+            return Response({"message": "You already liked this post."}, status=400)
 
 class UnlikePost(generics.DestroyAPIView):
-    queryset = Like.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_destroy(self, instance):
-        instance.delete()  # Remove the like
+    def delete(self, request, pk, *args, **kwargs):
+        post = get_object_or_404(Post, pk=pk)  # Retrieve the post
+        try:
+            like = Like.objects.get(user=request.user, post=post)  # Find the like
+            like.delete()  # Remove the like
+            return Response({"message": "Post unliked."}, status=204)
+        except Like.DoesNotExist:
+            return Response({"message": "You have not liked this post."}, status=400)
